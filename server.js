0290f5b73659dc92583c96db6b50e923c3ae15ed
Kavya Mantena <newtechkn@kavyas-air.lan>
2022-05-04 21:18:03 -0500

    ACreated the user login and

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const routes = require('./controllers');
+const session = require('express-session');
+
 const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const path = require('path');
@@ -7,6 +9,23 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+
+
 
 const hbs = exphbs.create({});
 
